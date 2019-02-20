4abe4ffaee449da33be8973721fd6a6f05ffa24a
guilhermepozo <guilhermepozoti@gmail.com>
2019-02-20 17:41:34 +0000

    utils

---
+++ utils/log.js
@@ -0,0 +1,19 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp(),
+    format.simple(),
+    format.json(),
+    format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`),
+    format.colorize({ all: true })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({filename:'logs/info.log', level:'info'}),
+    new transports.File({filename:'logs/error.log', level:'error'}),
+    new transports.File({filename:'logs/logs.log'}),
+  ]
+});
+
+module.exports = logger;

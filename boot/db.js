15766cccb72da6a0007195a22c738ed73cc8d998
guilhermepozo <guilhermepozoti@gmail.com>
2019-02-20 23:02:39 -0300

    Update db.js

--- boot/db.js
+++ boot/db.js
@@ -4,10 +4,7 @@ const logger = require('../utils/log')
 
 
 module.exports = function() {
-  const host = config.get('host');
-  const dbname = config.get('db_name');
-  const username = config.get('db_username');
-  const password = config.get('db_password');
-  mongoose.connect( `mongodb+srv://${username}:${password}@${host}/${dbname}?retryWrites=true`, { useNewUrlParser: true })
+  const db = config.get('db');
+  mongoose.connect(db, { useNewUrlParser: true })
     .then(() => logger.info(`Mongoose Conectado em MongoDB...`));
 }
